dabb4a29b0b32f43cffb6f531cdf1ded58a75255
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-06-27 14:48:50 -0300

    Initial Commit

---
+++ MusiQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusiQuiz
+//
+//  Created by Vinicius Mangueira on 27/06/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
